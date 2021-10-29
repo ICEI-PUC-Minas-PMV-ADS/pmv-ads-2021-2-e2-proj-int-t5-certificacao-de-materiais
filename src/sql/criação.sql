2de67f972215df81da6bbccb69886ae9b95243e1
Renan Gonçalves de Souza <renan.gsouza@pm.me>
2021-10-29 13:36:47 -0300

    RF-01: script sql: estrutura de relacionamento básica

--- src/sql/criação.sql
+++ src/sql/criação.sql
@@ -4,21 +4,31 @@ CREATE DATABASE CertMat;
 USE CertMat;
 
 CREATE TABLE Laboratorio (
-    CodLaboratorio int(3) NOT NULL AUTO_INCREMENT,
+    ID int(3) NOT NULL AUTO_INCREMENT,
     Nome varchar(255), 
     Usuario vachar(30), 
     Senha varchar(30),
 
-    PRIMARY KEY (CodLaboratorio)
+    PRIMARY KEY (ID)
 );
 
 CREATE TABLE Material (
-    CodigoMaterial int(3) NOT NULL AUTO_INCREMENT,
+    ID int(3) NOT NULL AUTO_INCREMENT,
     Nome varchar(255), 
     
-    PRIMARY KEY(CodMaterial)
+    PRIMARY KEY(ID)
 );
 
+-- Junction Laboratorio/Material
+
 CREATE TABLE Certificacao (
-    Nome varchar(255)
+    Laboratorio_ID int NOT NULL,
+    Material_ID int NOT NULL,
+    Nome varchar(255),
+
+    PRIMARY KEY(ID),
+    CONSTRAINT Certificacao_Laboratorio FOREIGN KEY (Laboratorio_ID) REFERENCES Laboratorio(ID),
+    CONSTRAINT Certificacao_Material FOREIGN KEY (Material_ID) REFERENCES Material(ID),
+    CONSTRAINT Certificacao_ID UNIQUE (Laboratorio_ID, Material_ID)
 );
+
