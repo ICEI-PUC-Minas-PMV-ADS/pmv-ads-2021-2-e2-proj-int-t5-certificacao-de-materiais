41fd55af3f856e8ff03aadd35a5ed67552873953
Renan Gonçalves de Souza <renan.gsouza@pm.me>
2021-10-29 12:15:52 -0300

    #18: estrutura básica do banco

---
+++ criação.sql
@@ -0,0 +1,24 @@
+-- Cria Base e Tabelas
+
+CREATE DATABASE CertMat;
+USER CertMat;
+
+CREATE TABLE Laboratorio (
+    CodLaboratorio int(3) NOT NULL AUTO_INCREMENT,
+    Nome varchar(255), 
+    Usuario vachar(30), 
+    Senha varchar(30),
+
+    PRIMARY KEY (CodLaboratorio)
+);
+
+CREATE TABLE Material (
+    CodigoMaterial int(3) NOT NULL AUTO_INCREMENT,
+    Nome varchar(255), 
+    
+    PRIMARY KEY(CodMaterial)
+);
+
+CREATE TABLE Certificacao (
+    Nome varchar(255)
+);
