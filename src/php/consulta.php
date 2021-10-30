9280ab0338901b51764cff333a8ff4f181e93f4c
Renan Gonçalves de Souza <renan.gsouza@pm.me>
2021-10-30 18:28:20 -0300

    RF-01: CRUD 100% funcional. closes #19

--- src/php/consulta.php
+++ src/php/consulta.php
@@ -5,6 +5,8 @@ $username = "renan";
 $password = "MySQL-SGBD@1989";
 $dbname = "CertMat";
 
+$material = $_POST["material"];
+
 // Cria conexão.
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -15,11 +17,12 @@ if ($conn->connect_error) {
 
 // Query.
 //$sql = "SELECT * FROM Laboratorio";
-$sql = "SELECT Nome, Contato FROM Laboratorio WHERE Nome IN (SELECT Laboratorio_Nome FROM Certificacao WHERE Material_Nome = 'Aço');";
+$sql = "SELECT Nome, Contato FROM Laboratorio WHERE Nome IN (SELECT Laboratorio_Nome FROM Certificacao WHERE Material_Nome = '" . $material . "');";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     
+    echo "<h2> Laboratórios cadastrados que emitem certificação para: " . $material;
     echo "<table border=1> <tr> <th> Laboratório </th> <th> Endereço </th> </tr>";
 
     // saída de cada linha.
