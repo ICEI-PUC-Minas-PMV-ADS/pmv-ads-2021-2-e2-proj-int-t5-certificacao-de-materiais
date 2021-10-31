202063e417bf1b611a68c8b47d1322f92e5e5f0a
Renan Gonçalves de Souza <renan.gsouza@pm.me>
2021-10-31 11:11:41 -0300

    diversos: estrutura funcional, integrado jquery...

---
+++ src/script/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#loadCons").click(function() {
+        $("#content").attr("data", "consulta.html");
+    });
+    $("#loadLabs").click(function() {
+        $("#content").attr("data", "labs.html");
+    });
+    $("#loadNews").click(function() {
+        $("#content").attr("data", "news.html");
+    });
+    $("#loadAbout").click(function() {
+        $("#content").attr("data", "about.html");
+    });
+
+    $(".clickable").mouseover(function () {
+        $(this).css("color", "white");
+        $(this).css("background-color", "rgb(204, 102, 0)");
+    });
+    $(".clickable").mouseleave(function () {
+        $(this).css("color", "white");
+        $(this).css("background-color", "rgb(255, 153, 102)");
+    });
+});
