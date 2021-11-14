9e687f07221fbdd995ac46b4e2cd20e20e434cef
Renan G Souza <57996436+renangsouza@users.noreply.github.com>
2021-11-14 18:24:55 -0300

    funcionalidade: valida se usuário existe no banco

--- src/cadastro.php
+++ src/cadastro.php
@@ -2,7 +2,7 @@
 // Injeta página que cuida da conexão com o banco.
 require_once "db_connect.php";
 
-// Variáveis de feedback. Armazenam text de apoio ao usuário.
+// Variáveis de feedback. Armazenam texto de apoio ao usuário.
 $username_err = $password_err = $confirmpass_err = '';
 
 // Processando os dados quando o formulário é enviado.
@@ -10,6 +10,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Tratamento do campo "Usuário".
     if (empty(trim($_POST["username"]))) {
         $username_err = "Usuário não pode ser vazio.";
+    } elseif (!preg_match("/^[a-zA-Z]+$/", trim($_POST["username"]))) {
+        $username_err = "Usuário só pode conter letras.";
+    } else {
+        // Checa se usuário existe no banco.
+        $sql = $mysqli->prepare("SELECT id FROM Usuario WHERE username = ?");
+        $sql->bind_param('s', trim($_POST["username"]));
+        
+        if ($sql->execute()) {
+            $sql->store_result();
+            if ($sql->num_rows == 1) {
+                $username_err = "Nome de usuário já em uso.";
+            }
+        } else {
+            echo "Falha ao consultar o banco."
+        }
+        
+        $sql->close();
     }
 
     //Tratamento do campo "Senha" e "Confirmar senha".
