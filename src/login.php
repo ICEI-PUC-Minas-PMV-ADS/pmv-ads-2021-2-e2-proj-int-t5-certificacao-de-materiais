8690c7c5a0abbf4aaceca417fa00d3cf36a4f0d9
Renan G Souza <57996436+renangsouza@users.noreply.github.com>
2021-11-15 11:23:47 -0300

    funcionalidade: esqueleto formulário de login

---
+++ src/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/subpages.css">
+        <meta chaset="UTF-8">
+    </head>
+    <body>
+        <div class="container">
+            <form>
+                <div class="form-line">
+                    <label>Usuário: </label>
+                    <input type="text">
+                </div>
+                <div class="form-line">
+                    <label>Senha: </label>
+                    <input type="password">
+                </div>
+                <div class="form-line">
+                    <input id="login-btn" class="clickable" type="submit" value="LOGAR">
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
