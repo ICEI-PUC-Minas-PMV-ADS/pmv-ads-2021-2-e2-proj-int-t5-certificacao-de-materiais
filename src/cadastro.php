45e5072efe9dd022b6fbaab81f6d2bfd5a928a61
Renan G Souza <57996436+renangsouza@users.noreply.github.com>
2021-11-14 21:06:37 -0300

    funcionalidade: cadastro de usuário

--- src/cadastro.php
+++ src/cadastro.php
@@ -5,6 +5,9 @@ require_once "db_connect.php";
 // Variáveis de feedback. Armazenam texto de apoio ao usuário.
 $username_err = $password_err = $confirmpass_err = '';
 
+// Variáveis para montagem da query.
+$username = $password = '';
+
 // Processando os dados quando o formulário é enviado.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Tratamento do campo "Usuário".
@@ -21,9 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $sql->store_result();
             if ($sql->num_rows == 1) {
                 $username_err = "Nome de usuário já em uso.";
+            } else {
+                $username = trim($_POST["username"]);
             }
         } else {
-            echo "Falha ao consultar o banco."
+            echo "ERRO!: falha ao consultar no banco.";
         }
         
         $sql->close();
@@ -32,10 +37,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Tratamento do campo "Senha" e "Confirmar senha".
     if (empty(trim($_POST["password"]))) {
         $password_err = "Senha não pode ser vazia.";
+    } elseif (strlen(trim($_POST[password])) < 8) {
+        $password_err = "Senha tem que ter pelo menos 8 caracteres.";
     } else {
         if (empty(trim($_POST["confirm_password"]))) {
-            $confirmpass_err = "Repita a senha para confirmação.";
+            $confirmpass_err = "Confirme a senha.";
+        } elseif (trim($_POST["password"]) != trim($_POST["confirm_password"])){
+            $confirmpass_err = "Valores informados estão diferentes.";
+        } else {
+            $password = trim($_POST["password"]);
         }
+    }
+
+    // Checa se tudo está aceitável para registrar novo usuário e registra.
+    if (!empty($username) && !empty($password)) {
+        $sql = $mysqli->prepare("INSERT INTO Usuario (username, password) VALUES (?, ?)");
+        $sql->bind_param("ss", $username, $password);
+
+        if ($sql->execute()) {
+            echo "OK!: teste de inserção finalizado com sucesso.";
+        } else {
+            echo "ERRO!: falha ao inserir no banco.";
+        }
+
+        $sql->close();
     }
 }
 
