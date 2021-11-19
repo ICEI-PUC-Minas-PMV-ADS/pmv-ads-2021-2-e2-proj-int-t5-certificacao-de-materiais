ddafdbafb6b6d051d24f5ff992f69fd4d8447762
Renan G Souza <57996436+renangsouza@users.noreply.github.com>
2021-11-18 22:52:48 -0300

    funcionalidade: associação entre perfil e lab

--- src/labs.php
+++ src/labs.php
@@ -1,17 +1,7 @@
 <?php 
 
 session_start();
-
-// Checa se o usuário já logou.
-if (isset($_SESSION["isLogged"]) && $_SESSION["isLogged"] === true) {
-    
-    echo "<p> Bem vindo, " . htmlspecialchars($_SESSION["username"]) . ".</p>";
-    echo "<p> <a href='logout.php'>Sair</a></p>";
-} else {
-    
-    echo "<p> Trabalha com emissão de certificados? <a href='cadastro.php'> Cadastre-se </a> para divulgar seus serviços. </p>";
-    echo "<p> Já tem um pefil conosco? <a href='login.php'> Faça login </a> para acessar sua conta.</p>";
-}
+require_once("db_connect.php");
 
 ?>
 
@@ -22,6 +12,38 @@ if (isset($_SESSION["isLogged"]) && $_SESSION["isLogged"] === true) {
         <meta charset="UTF-8">
     </head>
     <body>
-        <!-- -->
+        <?php
+        // Checa se o usuário já logou.
+        if (isset($_SESSION["isLogged"]) && $_SESSION["isLogged"] === true) {
+
+            echo "<p> Bem vindo, " . htmlspecialchars($_SESSION["username"]) . ".</p>";
+            echo "<p> <a href='logout.php'>Sair</a></p>";
+
+            // Verifica se há Laboratório associado à conta.
+            $stmt = $mysqli->prepare("SELECT id FROM Laboratorio WHERE usuario_username = ?");
+            $stmt->bind_param('s', $_SESSION["username"]);
+            $stmt->execute();
+            $stmt->store_result();
+
+            if ($stmt->num_rows == 1) {
+                
+                // TODO: Há laboratório cadastrado ao login. Direcionar direto para as certificações?
+
+            } else {
+
+                // TODO: Finalizar cadastro com informações de endereço. Página nova?
+                
+            }
+
+            $stmt->close();
+            $mysqli->close();
+
+        } else {
+            
+            echo "<p> Trabalha com emissão de certificados? <a href='join.php'> Cadastre-se </a> para divulgar seus serviços. </p>";
+            echo "<p> Já tem um pefil conosco? <a href='login.php'> Faça login </a> para acessar sua conta.</p>";
+
+        }
+        ?>
     </body>
 </html>
