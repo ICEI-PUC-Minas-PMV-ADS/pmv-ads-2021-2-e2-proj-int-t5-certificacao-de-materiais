ee0a1eb14cbdc593c2e696cefdab2f9304acfc47
iagogfnzy <iago.goncalves@yahoo.com>
2021-11-29 18:56:30 -0300

    feat: add Api C#

---
+++ src/api/LaboratorioCadastroAPI/Controllers/LaboratorioCadastroController.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using LaboratorioCadastroAPI.Entity;
+using LaboratorioCadastroAPI.Entity.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace LaboratorioCadastroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LaboratorioCadastroController : ControllerBase
+    {
+        private readonly IConnectionFactory connectionFactory;
+
+        public LaboratorioCadastroController(IConnectionFactory connectionFactory)
+        {
+            this.connectionFactory = connectionFactory;
+        }
+        [HttpPost]
+        [Route("cadastrar")]
+        public async Task<IActionResult> Post([FromBody] FormularioCadastro formularioCadastro)
+        {
+            var url = "INSERT INTO [LaboratorioDb].[dbo].[FormularioCadastro] ([NomeLaboratorio],[CNPJ],[AreiaQuantidade],[CimentoQuantidade],[FerroQuantidade],[Endereco])" +
+                "values" +
+                "(@NomeLaboratorio,@CNPJ,@AreiaQuantidade,@CimentoQuantidade,@FerroQuantidade,@Endereco)";
+
+            using (var connectionDb = connectionFactory.Connection())
+            {
+                connectionDb.Open();
+                var result = await connectionDb.ExecuteAsync(url,
+                    new
+                    {
+                        NomeLaboratorio = formularioCadastro.NomeLaboratorio,
+                        CNPJ = formularioCadastro.CNPJ,
+                        AreiaQuantidade = formularioCadastro.Areia,
+                        CimentoQuantidade = formularioCadastro.Cimento,
+                        FerroQuantidade = formularioCadastro.Ferro,
+                        Endereco = formularioCadastro.Endereco
+                    });
+
+                return Ok();
+
+            }
+        }
+
+        [HttpGet]
+        [Route("buscar")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var url = "SELECT * from [LaboratorioDb].[dbo].[FormularioCadastro]";
+
+            using (var connectionDb = connectionFactory.Connection())
+            {
+                connectionDb.Open();
+                var result = await connectionDb.QueryAsync<dynamic>(url);
+
+                return Ok();
+            }
+        }
+
+    }
+}
---
+++ src/api/LaboratorioCadastroAPI/Entity/FormularioCadastro.cs
@@ -0,0 +1,13 @@
+﻿namespace LaboratorioCadastroAPI.Entity
+{
+    public class FormularioCadastro
+    {
+        public string NomeLaboratorio { get; set; }
+        public string CNPJ { get; set; }
+        public int Areia { get; set; }
+        public int Cimento { get; set; }
+        public int Ferro { get; set; }
+        public string Endereco { get; set; }        
+
+    }
+}
