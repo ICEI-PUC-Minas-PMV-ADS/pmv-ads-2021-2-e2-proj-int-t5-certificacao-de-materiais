3d6df75cb001b675a8a691877b372d336afd4045
iagogfnzy <iago.goncalves@yahoo.com>
2021-11-29 22:23:32 -0300

    feat: add pasta API com codigo em C#

---
+++ src/api/LaboratorioCadastroAPI/Entity/DeafultSqlConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿using LaboratorioCadastroAPI.Entity.Interface;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LaboratorioCadastroAPI.Entity
+{
+    public class DeafultSqlConnectionFactory : IConnectionFactory
+    {
+        public IDbConnection Connection()
+        {
+            return new SqlConnection(@"Password=admin123;Persist Security Info=True;User ID=sa;Initial Catalog=LaboratorioDB;Data Source=DESKTOP-IGQJCON\SQLEXPRESS");
+        }
+    }
+}
+ 
---
+++ src/api/LaboratorioCadastroAPI/Entity/Interface/IConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace LaboratorioCadastroAPI.Entity.Interface
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection Connection();
+    }
+}
